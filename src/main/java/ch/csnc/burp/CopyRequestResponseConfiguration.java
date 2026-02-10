b5a4eaa7eb11cd612af36b8f5ed412cfe97ca6b9
Tobias Hort <tobias.hort-giess@compass-security.com>
2026-02-10 11:30:21 +0100

    make template configurable

--- src/main/java/ch/csnc/burp/CopyRequestResponseConfiguration.java
+++ src/main/java/ch/csnc/burp/CopyRequestResponseConfiguration.java
@@ -7,66 +7,62 @@ import burp.api.montoya.ui.settings.SettingsPanelWithData;
 
 public class CopyRequestResponseConfiguration {
 
+  private static final String CUT_TEXT_LABEL = "Cut Text";
+  private static final String CUT_TEXT_DEFAULT = "[...]";
+  private static final String COPY_FULL_FULL_OR_SELECTION_HOT_KEY_LABEL = "HotKey for Copy Full/Full or Full/Header + Selected Data";
+  private static final String COPY_FULL_FULL_OR_SELECTION_HOT_KEY_DEFAULT = "Ctrl+Shift+C";
+  private static final String COPY_FULL_HEADER_LABEL = "HotKey for Copy Full/Header";
+  private static final String COPY_FULL_HEADER_DEFAULT = "Ctrl+Shift+Alt+C";
+  private static final String USE_NBSP_LABEL = "Use Non-Breakable Spaces";
+  private static final boolean USE_NBSP_DEFAULT = false;
+  private static final String TEMPLATE_LABEL = "Template";
+  private static final String TEMPLATE_DEFAULT = "%s%n%n%s";
 
-    private static final String cutTextLabel = "Cut Text";
-    private static final String cutTextDefault = "[...]";
+  private final static SettingsPanelSetting[] settings = new SettingsPanelSetting[] {
+      SettingsPanelSetting.stringSetting(CUT_TEXT_LABEL, CUT_TEXT_DEFAULT),
+      SettingsPanelSetting.booleanSetting(USE_NBSP_LABEL, USE_NBSP_DEFAULT),
+      SettingsPanelSetting.stringSetting(COPY_FULL_FULL_OR_SELECTION_HOT_KEY_LABEL, COPY_FULL_FULL_OR_SELECTION_HOT_KEY_DEFAULT),
+      SettingsPanelSetting.stringSetting(COPY_FULL_HEADER_LABEL, COPY_FULL_HEADER_DEFAULT),
+      SettingsPanelSetting.stringSetting(TEMPLATE_LABEL, TEMPLATE_DEFAULT),
+  };
 
-    private static final String copyFullFullOrSelectionHotKeyLabel = "HotKey for Copy Full/Full or Full/Header + Selected Data";
-    private static final String copyFullFullOrSelectionHotKeyDefault = "Ctrl+Shift+C";
+  private final static SettingsPanelWithData panel = SettingsPanelBuilder.settingsPanel()
+      .withPersistence(SettingsPanelPersistence.USER_SETTINGS)
+      .withTitle("CopyRequestResponse Configuration")
+      .withDescription("The extension's behavior can be customized here.")
+      .withKeywords("CopyRequestResponse", "Settings", "CutText")
+      .withSettings(settings)
+      .build();
 
-    private static final String copyFullHeaderLabel = "HotKey for Copy Full/Header";
-    private static final String copyFullHeaderDefault = "Ctrl+Alt+C";
-
-    private static final String useNbspCheckbox = "Use Non-Breakable Spaces";
-    private static final boolean useNbspCheckboxDefault = false;
-
-    public static String cutText() {
-        var cutText = panel.getString(cutTextLabel);
-
-        if (useNonBreakableSpace()) {
-            cutText = cutText.replaceAll(" ", "\u00a0");
-        }
-        return cutText;
+  public static String cutText() {
+    var cutText = panel.getString(CUT_TEXT_LABEL);
+    if (useNonBreakableSpace()) {
+      cutText = cutText.replaceAll(" ", "\u00a0");
     }
+    return cutText;
+  }
 
+  public static boolean useNonBreakableSpace() {
+    return panel.getBoolean(USE_NBSP_LABEL);
+  }
 
-    public static boolean useNonBreakableSpace() {
-        return panel.getBoolean(useNbspCheckbox);
-    }
+  public static String copyFullFullOrSelectionHotKey() {
+    return panel.getString(COPY_FULL_FULL_OR_SELECTION_HOT_KEY_LABEL);
+  }
 
+  public static String copyFullHeaderHotKey() {
+    return panel.getString(COPY_FULL_HEADER_LABEL);
+  }
 
-    public static String copyFullFullOrSelectionHotKey() {
-        return panel.getString(copyFullFullOrSelectionHotKeyLabel);
-    }
+  public static String template() {
+    return panel.getString(TEMPLATE_LABEL);
+  }
 
+  public static void register() {
+    CopyRequestResponseExtension.api().userInterface().registerSettingsPanel(panel);
+  }
 
-    public static String copyFullHeaderHotKey() {
-        return panel.getString(copyFullHeaderLabel);
-    }
-
-
-    private CopyRequestResponseConfiguration() {
-        // static class
-    }
-
-    // Define setting panel entries
-    private final static SettingsPanelSetting[] settings = new SettingsPanelSetting[] {
-            SettingsPanelSetting.stringSetting(cutTextLabel, cutTextDefault),
-            SettingsPanelSetting.booleanSetting(useNbspCheckbox, useNbspCheckboxDefault),
-            SettingsPanelSetting.stringSetting(copyFullFullOrSelectionHotKeyLabel, copyFullFullOrSelectionHotKeyDefault),
-            SettingsPanelSetting.stringSetting(copyFullHeaderLabel, copyFullHeaderDefault)
-    };
-
-    // Create settings panel
-    private final static SettingsPanelWithData panel = SettingsPanelBuilder.settingsPanel()
-                                                                           .withPersistence(SettingsPanelPersistence.USER_SETTINGS)
-                                                                           .withTitle("CopyRequestResponse Configuration")
-                                                                           .withDescription("The extension's behavior can be customized here.")
-                                                                           .withKeywords("CopyRequestResponse", "Settings", "CutText")
-                                                                           .withSettings(settings)
-                                                                           .build();
-
-    public static void register() {
-        CopyRequestResponseExtension.api().userInterface().registerSettingsPanel(panel);
-    }
+  private CopyRequestResponseConfiguration() {
+    // static class
+  }
 }
--- src/main/java/ch/csnc/burp/CopyRequestResponseCopyActions.java
+++ src/main/java/ch/csnc/burp/CopyRequestResponseCopyActions.java
@@ -1,14 +1,10 @@
 package ch.csnc.burp;
 
-import burp.api.montoya.http.handler.TimingData;
 import burp.api.montoya.http.message.HttpRequestResponse;
 import burp.api.montoya.http.message.responses.HttpResponse;
 import burp.api.montoya.ui.contextmenu.MessageEditorHttpRequestResponse;
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -16,131 +12,127 @@ import java.util.stream.Collectors;
 
 public class CopyRequestResponseCopyActions {
 
-    public static void copyFullFull(List<HttpRequestResponse> requestResponses) {
-        var text =
-                requestResponses
-                        .stream()
-                        .map(requestResponse ->
-                                "%s\n\n%s".formatted(
-                                        requestResponse.request().toString().strip(),
-                                        Optional.ofNullable(requestResponse.response())
-                                                .map(HttpResponse::toString)
-                                                .map(String::strip)
-                                                .orElse("")))
-                        .collect(Collectors.joining("\n\n"));
+  public static void copyFullFull(List<HttpRequestResponse> requestResponses) {
+    var text = requestResponses
+        .stream()
+        .map(requestResponse -> CopyRequestResponseConfiguration.template().formatted(
+            requestResponse.request().toString().strip(),
+            Optional.ofNullable(requestResponse.response())
+                .map(HttpResponse::toString)
+                .map(String::strip)
+                .orElse("")))
+        .collect(Collectors.joining("\n\n"));
 
-        toClipboard(text);
-    }
+    toClipboard(text);
+  }
 
-    public static void copyFullHeader(List<HttpRequestResponse> requestResponses) {
-        var text =
-                requestResponses
-                        .stream()
-                        .map(requestResponse -> {
-                            var requestString = requestResponse.request().toString().strip();
+  public static void copyFullHeader(List<HttpRequestResponse> requestResponses) {
+    var text = requestResponses
+        .stream()
+        .map(requestResponse -> {
+          var requestString = requestResponse.request().toString().strip();
 
-                            var responseString = "";
-                            if (requestResponse.hasResponse()) {
-                                var response = requestResponse.response();
-                                responseString = response.toString().substring(0, response.bodyOffset()).strip();
-                                responseString += "\n\n";
-                                responseString += CopyRequestResponseConfiguration.cutText();
-                            }
-
-                            return "%s\n\n%s".formatted(requestString, responseString);
-                        })
-                        .collect(Collectors.joining("\n\n"));
-
-        toClipboard(text);
-    }
-
-    public static void copyFullHeaderPlusSelectedData(MessageEditorHttpRequestResponse editor) {
-        var requestResponse = editor.requestResponse();
-        var requestString = requestResponse.request().toString().strip();
-
-        Supplier<String> responseStringSupplier = () -> {
-            if (!requestResponse.hasResponse()) {
-                return "";
-            }
-
+          var responseString = "";
+          if (requestResponse.hasResponse()) {
             var response = requestResponse.response();
-            var responseString = response.toString().substring(0, response.bodyOffset()).strip();
+            responseString = response.toString().substring(0, response.bodyOffset()).strip();
             responseString += "\n\n";
-
-
-            var selectionOffsets = editor.selectionOffsets().orElse(null);
-
-            if (selectionOffsets == null) {
-                // nothing selected
-                responseString += CopyRequestResponseConfiguration.cutText();
-                return responseString;
-            }
-
-            var startIndex = selectionOffsets.startIndexInclusive();
-            if (startIndex < response.bodyOffset()) {
-                startIndex = response.bodyOffset();
-            }
-
-            var endIndex = selectionOffsets.endIndexExclusive();
-            CopyRequestResponseExtension.api().logging().logToError("start: %d, end %d".formatted(startIndex, endIndex));
-            if (endIndex <= startIndex) {
-                responseString += CopyRequestResponseConfiguration.cutText();
-                return responseString;
-            }
-
-            var selectedText = response.toByteArray().subArray(startIndex, endIndex);
-
-            if (startIndex == response.bodyOffset() && endIndex < response.toByteArray().length()) {
-                responseString += selectedText;
-                responseString += CopyRequestResponseConfiguration.cutText();
-                return responseString;
-            }
-
-            if (startIndex > response.bodyOffset() && endIndex == response.toByteArray().length()) {
-                responseString += CopyRequestResponseConfiguration.cutText();
-                responseString += selectedText;
-                return responseString;
-            }
-
-            if (startIndex == response.bodyOffset() && endIndex == response.toByteArray().length()) {
-                responseString += selectedText;
-                return responseString;
-            }
-
             responseString += CopyRequestResponseConfiguration.cutText();
-            responseString += selectedText;
-            responseString += CopyRequestResponseConfiguration.cutText();
-            return responseString;
-        };
+          }
 
-        var text = "%s\n\n%s".formatted(requestString, responseStringSupplier.get());
+          return CopyRequestResponseConfiguration.template().formatted(requestString, responseString);
+        })
+        .collect(Collectors.joining("\n\n"));
 
-        // Ugly hack because VMware is messing up the clipboard if a text is still selected, the function
-        // has to be run in a separate thread which sleeps for 0.2 seconds.
-        var thread = new Thread(() -> {
-            try {
-                Thread.sleep(200);
-                toClipboard(text);
-            } catch (InterruptedException exc) {
-                Thread.currentThread().interrupt();
-            }
-        });
-        thread.setName("ToClipboardThread");
-        thread.setDaemon(true);
-        thread.start();
-    }
+    toClipboard(text);
+  }
 
-    private static void toClipboard(String text0) {
-        var text1 = text0.replaceAll("\r\n", "\n");
-        var systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        var systemSelection = Toolkit.getDefaultToolkit().getSystemSelection();
-        var transferText = new StringSelection(text1);
-        systemClipboard.setContents(transferText, null);
-        systemSelection.setContents(transferText, null);
-    }
+  public static void copyFullHeaderPlusSelectedData(MessageEditorHttpRequestResponse editor) {
+    var requestResponse = editor.requestResponse();
+    var requestString = requestResponse.request().toString().strip();
 
-    private CopyRequestResponseCopyActions() {
-        // static class
-    }
+    Supplier<String> responseStringSupplier = () -> {
+      if (!requestResponse.hasResponse()) {
+        return "";
+      }
 
+      var response = requestResponse.response();
+      var responseString = response.toString().substring(0, response.bodyOffset()).strip();
+      responseString += "\n\n";
+
+      var selectionOffsets = editor.selectionOffsets().orElse(null);
+
+      if (selectionOffsets == null) {
+        // nothing selected
+        responseString += CopyRequestResponseConfiguration.cutText();
+        return responseString;
+      }
+
+      var startIndex = selectionOffsets.startIndexInclusive();
+      if (startIndex < response.bodyOffset()) {
+        startIndex = response.bodyOffset();
+      }
+
+      var endIndex = selectionOffsets.endIndexExclusive();
+      CopyRequestResponseExtension.api().logging().logToError("start: %d, end %d".formatted(startIndex, endIndex));
+      if (endIndex <= startIndex) {
+        responseString += CopyRequestResponseConfiguration.cutText();
+        return responseString;
+      }
+
+      var selectedText = response.toByteArray().subArray(startIndex, endIndex);
+
+      if (startIndex == response.bodyOffset() && endIndex < response.toByteArray().length()) {
+        responseString += selectedText;
+        responseString += CopyRequestResponseConfiguration.cutText();
+        return responseString;
+      }
+
+      if (startIndex > response.bodyOffset() && endIndex == response.toByteArray().length()) {
+        responseString += CopyRequestResponseConfiguration.cutText();
+        responseString += selectedText;
+        return responseString;
+      }
+
+      if (startIndex == response.bodyOffset() && endIndex == response.toByteArray().length()) {
+        responseString += selectedText;
+        return responseString;
+      }
+
+      responseString += CopyRequestResponseConfiguration.cutText();
+      responseString += selectedText;
+      responseString += CopyRequestResponseConfiguration.cutText();
+      return responseString;
+    };
+
+    var text = CopyRequestResponseConfiguration.template().formatted(requestString, responseStringSupplier.get());
+
+    // Ugly hack because VMware is messing up the clipboard if a text is still
+    // selected, the function
+    // has to be run in a separate thread which sleeps for 0.2 seconds.
+    var thread = new Thread(() -> {
+      try {
+        Thread.sleep(200);
+        toClipboard(text);
+      } catch (InterruptedException exc) {
+        Thread.currentThread().interrupt();
+      }
+    });
+    thread.setName("ToClipboardThread");
+    thread.setDaemon(true);
+    thread.start();
+  }
+
+  private static void toClipboard(String text0) {
+    var text1 = text0.replaceAll("\r\n", "\n");
+    var systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    var systemSelection = Toolkit.getDefaultToolkit().getSystemSelection();
+    var transferText = new StringSelection(text1);
+    systemClipboard.setContents(transferText, null);
+    systemSelection.setContents(transferText, null);
+  }
+
+  private CopyRequestResponseCopyActions() {
+    // static class
+  }
 }
